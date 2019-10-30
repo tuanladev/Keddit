fb3013aacdd56dfe0607b8f4bf1bdfe8c88b78d1
tuanla.dev <tuanla.dev@gmail.com>
2019-10-30 16:31:14 +0700

    Thêm 1 extensions mới

--- app/src/main/java/com/tuanlat/keddit/commons/extensions/Extensions.kt
+++ app/src/main/java/com/tuanlat/keddit/commons/extensions/Extensions.kt
@@ -5,6 +5,8 @@
 //tao class utils
 package com.tuanlat.keddit.commons.extensions
 
+import android.os.Parcel
+import android.os.Parcelable
 import android.text.TextUtils
 import android.view.LayoutInflater
 import android.view.View
@@ -32,3 +34,11 @@ fun ImageView.loadImg(imageUrl: String) {
         Picasso.with(context).load(imageUrl).into(this)
     }
 }
+
+// Inline function to create Parcel Creator
+inline fun <reified T : Parcelable> createParcel(
+    crossinline createFromParcel: (Parcel) -> T?): Parcelable.Creator<T> =
+    object : Parcelable.Creator<T> {
+        override fun createFromParcel(source: Parcel): T? = createFromParcel(source)
+        override fun newArray(size: Int): Array<out T?> = arrayOfNulls(size)
+    }
